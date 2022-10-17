b8a21b9174ea73ee033408bc47e0caf91596c14b
Nanga <nangahlangu7@gmail.com>
2022-10-17 11:55:53 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *
+ * main - print text using printf
+ *
+ *
+ *
+ * Return: 0
+ *
+*/
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
