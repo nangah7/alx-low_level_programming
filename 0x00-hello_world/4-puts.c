90023ef30c0763a3eb45efae05de1e2d743265e3
Nanga <nangahlangu7@gmail.com>
2022-10-17 11:44:27 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
+/**
  *main - print text using puts instead of printf
  *
  * Return 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
