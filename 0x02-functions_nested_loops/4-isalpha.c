bcffdecc3104600ec1578aac5886a652b8e5cd3b
Nanga <nangahlangu7@gmail.com>
2022-10-18 13:45:03 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"0
+/**
+ * _isalpha - function that checks for alphabetic character
+ * @c: variable checked
+ * Return: 1 else 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
