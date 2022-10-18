c5902846f38f927ae761fce6904b008603ab481f
Nanga <nangahlangu7@gmail.com>
2022-10-18 12:40:34 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-void pr_Alphabet(void);
-void pr_Alphabet_x10(void);
-int is_Lower(int c);
-int is_Alpha(int c);
-int pr_Sign(int n);
-int abs(int);
-int pr_LastDigit(int);
-void jackBauer(void);
-void timesTable(void);
-int add(int,, int);
-void prTo_98(int n);
+int _putchar(char c);
 
 #endif
